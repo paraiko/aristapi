9362ab034d22b9ad57c6e3b2f9bccf0dee149913
A.S. Boerema <paraiko@gmail.com>
2019-04-03 11:30:59 +0200

    Create temp_int.py
    initial commit of temperatur monitoring script

---
+++ temp_int.py
@@ -0,0 +1,14 @@
+from subprocess import check_output
+from re import findall
+from time import sleep, strftime, time
+def get_CPUtemp():
+		CPUtemp=check_output([“vcgencmd”,”measure_temp”]).decode(“UTF-8”)
+		CPUtemp=float(findall(“\d+\.\d+”,CPUtemp)[0])
+		return(CPUtemp)
+def write_CPUtemp(CPUtemp):
+		with open(“/dev/shm/mjpeg/user_annotate.txt”,”w”) as log:
+		  log.write(“{0}{1}{2}”.format(“CPU: “,str(CPUtemp),”C “))
+while True:
+		CPUtemp = get_CPUtemp()
+		write_CPUtemp(CPUtemp)
+		sleep(10) 
